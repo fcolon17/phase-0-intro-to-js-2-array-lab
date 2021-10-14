190880ed20ec3bb202f86b9e9cab9b69ba14aecf
Franklyn Colon <franklyncolon6@gmail.com>
2021-10-14 11:30:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(allCats) {
+  allCats = [...cats, "Broom"];
+}
